d6ea34c5c6aca5534f9d543d7e94ee249f9ccf2b
EfrenDavidLopezMancilla <101757727+EfrenDavidLopezMancilla@users.noreply.github.com>
2024-11-10 23:50:24 -0700

    IU

--- consulta.py
+++ consulta.py
@@ -1,21 +1,18 @@
-import psycopg2
+import os
+from psycopg2 import connect
 
-# URL de la base de datos
-db_url = "postgresql://efren:bjzyeuC0VXwWbO5tOug4sgNP32FGCrZC@dpg-cshd11u8ii6s73bcupag-a.oregon-postgres.render.com/bases_16pk"
+# Obtener los valores de las variables de entorno
+db_url = os.getenv('DATABASE_URL')
 
-# Conectar a la base de datos
 try:
     conn = psycopg2.connect(db_url)
     cursor = conn.cursor()
 
-    # Realizar una consulta (ajusta la consulta a tu caso)
     query = "SELECT * FROM estudiantes;"
     cursor.execute(query)
 
-    # Obtener los resultados
     estudiantes = cursor.fetchall()
 
-    # Mostrar los resultados
     print("Datos obtenidos de la base de datos:")
     for estudiante in estudiantes:
         print(estudiante)
@@ -23,7 +20,6 @@ try:
 except Exception as e:
     print(f"Ocurrió un error: {e}")
 finally:
-    # Cerrar la conexión
     if conn:
         cursor.close()
         conn.close()
